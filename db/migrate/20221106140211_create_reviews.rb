97b837036a2969af10b75bf548274e28e400cb02
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-11-06 20:14:16 +0300

    initial commit

---
+++ db/migrate/20221106140211_create_reviews.rb
@@ -0,0 +1,10 @@
+class CreateReviews < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reviews do |t|
+      t.integer :star_rating
+      t.string :comment
+      t.integer :product_id
+      t.integer :integer
+    end
+  end
+end
